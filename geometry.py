c161f628e9159f72d68b8400ed49b375c15532ff
BeataNowakowska <the.beata.nowakowska@gmail.com>
2024-12-08 22:34:57 +0100

    Komentarze na angielski

--- geometry.py
+++ geometry.py
@@ -5,65 +5,51 @@ class Geometry:
         pass
 
     def rectangle_area(self, width, height):
-        """Oblicza pole prostokąta"""
+        """Calculates the area of a rectangle"""
         return width * height
 
     def rectangle_perimeter(self, width, height):
-        """Oblicza obwód prostokąta"""
+        """Calculates the perimeter of a rectangle"""
         return 2 * (width + height)
 
     def circle_area(self, radius):
-        """Oblicza pole koła"""
+        """Calculates the area of a circle"""
         return math.pi * radius ** 2
 
     def circle_circumference(self, radius):
-        """Oblicza obwód koła"""
+        """Calculates the circumference of a circle"""
         return 2 * math.pi * radius
 
     def triangle_area(self, base, height):
-        """Oblicza pole trójkąta"""
+        """Calculates the area of a triangle"""
         return 0.5 * base * height
 
     def triangle_perimeter(self, a, b, c):
-        """Oblicza obwód trójkąta"""
+        """Calculates the perimeter of a triangle"""
         return a + b + c
 
     def square_area(self, side):
-        """Oblicza pole kwadratu"""
+        """Calculates the area of a square"""
         return side ** 2
 
     def square_perimeter(self, side):
-        """Oblicza obwód kwadratu"""
+        """Calculates the perimeter of a square"""
         return 4 * side
 
     def trapezoid_area(self, a, b, height):
-        """Oblicza pole trapezu"""
+        """Calculates the area of a trapezoid"""
         return 0.5 * (a + b) * height
 
     def trapezoid_perimeter(self, a, b, c, d):
-        """Oblicza obwód trapezu"""
+        """Calculates the perimeter of a trapezoid"""
         return a + b + c + d
 
 # Przykład użycia
 geometry = Geometry()
-print("Przykład użycia klasy")
 
 # Prostokąt
 print("Pole prostokąta:", geometry.rectangle_area(5, 10))
 print("Obwód prostokąta:", geometry.rectangle_perimeter(5, 10))
 
 # Koło
-print("Pole koła:", geometry.circle_area(7))
-print("Obwód koła:", geometry.circle_circumference(7))
-
-# Trójkąt
-print("Pole trójkąta:", geometry.triangle_area(6, 8))
-print("Obwód trójkąta:", geometry.triangle_perimeter(3, 4, 5))
-
-# Kwadrat
-print("Pole kwadratu:", geometry.square_area(4))
-print("Obwód kwadratu:", geometry.square_perimeter(4))
-
-# Trapez
-print("Pole trapezu:", geometry.trapezoid_area(5, 10, 6))
-print("Obwód trapezu:", geometry.trapezoid_perimeter(5, 10, 4, 7))
+print("Pole koła:", geometry.circle_area(
